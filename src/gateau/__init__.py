dd2d3848ddc0878e318a727880cdebe116adb21b
maybetree <maybetree48@proton.me>
2025-08-07 11:40:22 +0200

    ruff non-automatic fixes

--- src/gateau/__init__.py
+++ src/gateau/__init__.py
@@ -1,3 +1,14 @@
 from gateau import bindings
 from gateau import utilities
 
+# __all__ controls what `from gateau import *` actually does
+# but here we specify it to tell mypy
+# that everything here is imported for namespace flattening,
+# and not just on accident
+
+__all__ = [
+    'bindings',
+    'utilities'
+    ]
+
+
