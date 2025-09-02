444844783c866190b7bad32729dec7d294159882
maybetree <maybetree48@proton.me>
2025-09-02 12:20:22 +0200

    Make generatedocs.py less awful

--- GenerateDocs.py
+++ GenerateDocs.py
@@ -5,50 +5,96 @@ PyPO docs generator.
 For this script to work properly, you should have installed the docs prerequisites.
 """
 
-import os
+from pathlib import Path
 import shutil
+import subprocess
 import traceback
 
-def GenerateDocs():
-    docPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "docs")
-    demoPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.join("etc", "demos"))
-    doxyPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "doxy")
-    try:
+# Paths/names of command-line tools
+DOXYGEN = "doxygen"
+JUPYTER = "jupyter"
+
+def check_deps():
+    for call in ((DOXYGEN, "--version"), (JUPYTER, "--version")):
         try:
-            shutil.rmtree(docPath)
-        except Exception:
-            print(traceback.format_exc())
+            subprocess.run(
+                call,
+                shell=False,
+                check=True,
+                )
+        except (subprocess.CalledProcessError, FileNotFoundError) as err:
+            raise Exception(
+                f"External dependency `{call[0]}` is missing or broken, "
+                "go fix it!"
+                ) from err
 
-        # Convert demos to html format for inclusion in the documentation.
-        for (dirpath, dirnames, filenames) in os.walk(demoPath):
-            destPath = os.path.join(docPath, "demos")
-            os.makedirs(destPath)
-            for file in filenames:
-                if file.split('.')[1] != "ipynb":
-                    continue
 
-                _path = os.path.join(demoPath, file)
-                htmlPath = os.path.join(demoPath, f"{file.split('.')[0]}.html")
-                html_destPath = os.path.join(destPath, f"{file.split('.')[0]}.html")
 
-                os.system(f"jupyter nbconvert --to html --template lab --theme dark {_path}")
-                os.rename(htmlPath, html_destPath)
-            
-            break
 
-        os.system(f"doxygen {os.path.join(doxyPath, 'Doxyfile')}")
-        
-        filelistPath = os.path.join(docPath, "files.html")
-        with open(filelistPath, 'r') as file :
-            filedata = file.read()
-            filedata = filedata.replace('File List', 'Full Software Documentation')
-            filedata = filedata.replace("Here is a list of all documented files with brief descriptions:",
-                                        "Here is a list containing the full software documentation. The structure of this page reflects the source code hierarchy.")
-        with open(filelistPath, 'w') as file:
-            file.write(filedata)
-        
-    except Exception:
-        print(traceback.format_exc())
+def generate_docs():
+    base_path = Path(__file__).resolve().parent
+    doc_path = base_path / "docs"
+    demo_path = base_path / "etc" / "demos"
+    doxy_path = base_path / "doxy"
+
+    shutil.rmtree(doc_path)
+
+    ### Convert jupyter to html ###
+
+    for file in demo_path.iterdir():
+        if not (file.is_file() and file.suffix == ".ipynb"):
+            continue
+
+        dest_path = doc_path / "demos"
+        dest_path.mkdir(parents=True, exist_ok=True)
+
+        html_path = demo_path / f"{file.stem}.html"
+        html_dest_path = dest_path / html_path.name
+
+        subprocess.run(
+            [
+                JUPYTER,
+                "nbconvert",
+                "--to",
+                "html",
+                "--template",
+                "lab",
+                "--theme",
+                "dark",
+                str(file),
+                ],
+            shell=False,
+            check=True
+        )
+
+        html_path.rename(html_dest_path)
+
+    ### Run doxy ###
+
+    subprocess.run(
+        [
+            DOXYGEN,
+            str(doxy_path / "Doxyfile"),
+            ],
+        shell=False,
+        check=True
+        )
+
+    ### Fix filelist ###
     
+    filelist_path = doc_path / "files.html"
+
+    if filelist_path.exists():
+        # TODO better way to do this??
+        content = filelist_path.read_text()
+        content = content.replace('File List', 'Full Software Documentation')
+        content = content.replace(
+            "Here is a list of all documented files with brief descriptions:",
+            "Here is a list containing the full software documentation. The structure of this page reflects the source code hierarchy."
+        )
+        filelist_path.write_text(content)
+
 if __name__ == "__main__":
-    GenerateDocs()
+    check_deps()
+    generate_docs()
+
