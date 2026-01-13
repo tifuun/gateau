ab1e56811cfc267b4a22de77e742b02c2c4c246c
maybetree <maybetree48@proton.me>
2026-01-13 12:36:18 +0100

    add cli entrypoint for selftest

---
+++ src/gateau/__main__.py
@@ -0,0 +1,15 @@
+"""!
+@file __main__.py
+@brief Gateau cli entry point (for selftest)
+"""
+
+from gateau import selftest
+
+def main():
+    print('RUNNING GATEAU SELFTEST')
+    selftest()
+
+if __name__ == '__main__':
+    main()
+
+
