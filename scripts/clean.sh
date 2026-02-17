aab7dea1a5d8e402676c93aa61321338bd932381
maybetree <maybetree@stratal.systems>
2026-02-17 19:25:28 +0100

    update clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,17 +7,17 @@ rm -rf \
 	.ruff_cache \
 	.skbuild-info.json \
 	CMakeCache.txt \
-	CMakeCache.txt \
 	CMakeFiles \
-	CMakeFiles/ \
 	CMakeInit.txt \
-	System\ Volume\ Information/ \
 	Makefile \
+	System\ Volume\ Information/ \
 	\$RECYCLE.BIN/ \
 	build \
-	build/ \
+	build.win \
+	build.docker \
+	build.dk \
+	wheelhouse \
 	dist \
-	dist/ \
 	docs \
 	gateau_test \
 	gsl \
@@ -26,9 +26,15 @@ rm -rf \
 	requirements.txt.bak \
 	src/gateau/libgateau.so \
 	testfile \
+	tmp \
 	venv/ \
 	wheel/ \
-	win/ \
+	*.dll \
+	vcpkg_installed \
+	.mesonpy* \
 
+find . -name '__pycache__' -exec rm -rf {} \;
+find . -name '*.pyc' -exec rm -rf {} \;
+find . -name '*.pyo' -exec rm -rf {} \;
 
 
